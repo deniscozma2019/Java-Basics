b7c253a65d7caf022ac384bf5f39bd07814b93c4
denicozma2019 <denis.cozma2015@gmail.com>
2023-03-02 16:51:29 +0200

    input console

---
+++ src/Console/Input.java
@@ -0,0 +1,11 @@
+package Console;
+
+public class Input {
+    public static void main (String[] args){
+        System.out.println("Hello World");
+        System.out.println("Bye World");
+        System.out.print("Hello world!");
+        System.out.print("Hello world \n");
+        System.out.print("Bye world...");
+    }
+}
