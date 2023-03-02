7232aaba5d86c8d392c300599ca985fc1b2b421a
denicozma2019 <denis.cozma2015@gmail.com>
2023-03-02 15:37:49 +0200

    data types

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,30 @@
 import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.print("Input a number: ");
-        int num = in.nextInt();
-        if (num==1) {
-            System.out.println("este egal cu unu");
-            }
-        else{
-            System.out.println("nu este egal cu unu");
-        }
+        int x = 10;
+        var y = 1500000000;
+        final int z = 10;
+        boolean istrue = true;
+        boolean isfalse = false;
+        byte a = 55;
+        short b = 3;
+        long c = 5;
+        double d = 8.5;
+        float e = 8.5F;
+        char ch=102;
+        String hello = "Hello...";
+        String text = "Вот мысль, которой весь я предан,\n"+
+                "Итог всего, что ум скопил.\n"+
+                "Лишь тот, кем бой за жизнь изведан,\n"+
+                "Жизнь и свободу заслужил.";
+        System.out.println(text);
+
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
+        System.out.println(istrue);
+        System.out.println(isfalse);
+        System.out.println(ch);
+        System.out.println(hello);
     }
 }
