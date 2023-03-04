da5a088297aa5b45d6c23c9563b369ef06ca9541
denicozma2019 <denis.cozma2015@gmail.com>
2023-03-04 09:43:29 +0200

    input console

---
+++ src/OOP/Study.java
@@ -0,0 +1,11 @@
+package OOP;
+
+public class Study {
+    public static void main (String[] args){
+        int with = 5;
+      int height = 10;
+      int result= with*height;
+        System.out.println(result);
+    }
+}
+
