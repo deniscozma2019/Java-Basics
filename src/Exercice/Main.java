9ce0e18deb177928485fbf57449f105b822dd78e
denicozma2019 <denis.cozma2015@gmail.com>
2023-03-06 23:53:30 +0200

    ex1

---
+++ src/Exercice/Main.java
@@ -0,0 +1,20 @@
+package Exercice;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        int ani, zile,luni,ore;
+        Scanner input = new Scanner(System.in);
+        System.out.println("introduceti numarul de ani");
+        ani= input.nextInt();
+
+        luni=ani * 12;
+        zile = ani * 365;
+        ore = zile * 24;
+        System.out.println(ani + " ani echivalează cu:");
+        System.out.println(luni + " luni");
+        System.out.println(zile + " zile");
+        System.out.println(ore + " ore");
+    }
+}
--- src/OOP/Study.java
+++ src/OOP/Study.java
@@ -2,9 +2,7 @@ package OOP;
 
 public class Study {
     public static void main (String[] args){
-        int with = 5;
-      int height = 10;
-      int result= with*height;
+        int with = 5,height = 10,result= with*height;
         System.out.println(result);
     }
 }
