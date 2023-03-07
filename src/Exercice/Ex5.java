956a7a3fc63a0af2358d7eb177fd699cf887cdd2
denicozma2019 <denis.cozma2015@gmail.com>
2023-03-07 16:01:56 +0200

    ex5

---
+++ src/Exercice/Ex5.java
@@ -0,0 +1,16 @@
+package Exercice;
+
+import java.util.Scanner;
+
+public class Ex5 {
+    public static void main(String[]args){
+        int x;
+        int first;
+        int result=0;
+        System.out.println("introduceti numarul");
+         Scanner input= new Scanner(System.in);
+        x=input.nextInt();
+        result += (x % 1000) + (x % 100) + (x % 10);
+        System.out.println("Suma numerelor obținute prin eliminarea cifrelor este: " + result);
+    }
+}
