9a7c5f93668690d20fdf7a5058241fa1c70515a9
deniscozma2019 <denis.cozma2015@gmail.com>
2023-04-11 22:47:15 +0300

    Lab 4 ex 1,3

---
+++ src/Exercice/Basic/Lab4/Ex3.java
@@ -0,0 +1,26 @@
+package Exercice.Basic.Lab4;
+
+import java.util.Scanner;
+
+public class Ex3 {
+    public static void main(String[] args) {
+        int natural ;
+        double real;
+        System.out.println("Introduceti numarul natural");
+        Scanner natural_number = new Scanner(System.in);
+        natural = natural_number.nextInt();
+
+        System.out.println("Introduceti numarul real");
+        Scanner real_number = new Scanner(System.in);
+        real = real_number.nextDouble();
+
+
+
+    }
+}
+/*
+* Se citește un număr natural n și apoi n numere reale. Să se elaboreze un program
+care calculează și afișează raportul dintre suma numerelor impare și suma numerelor
+pare.
+* */
+// huio znaet cum de rezolvat
---
+++ src/Exercice/Basic/Lab4/Ex4.java
@@ -0,0 +1,7 @@
+package Exercice.Basic.Lab4;
+
+public class Ex4 {
+    public static void main(String[] args) {
+
+    }
+}
