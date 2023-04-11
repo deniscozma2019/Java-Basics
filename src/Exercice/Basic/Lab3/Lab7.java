6c9ed9d343086e3badee96c09cba1b12a9950559
deniscozma2019 <denis.cozma2015@gmail.com>
2023-04-11 15:07:35 +0300

    Lab 3 ex 7

---
+++ src/Exercice/Basic/Lab3/Lab7.java
@@ -0,0 +1,27 @@
+package Exercice.Basic.Lab3;
+
+import java.util.Scanner;
+
+public class Lab7 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Introduceti numarul");
+        int number = scanner.nextInt();
+        int sute = number/100;
+        int zeci = (number/10)%10;
+        int unitati =  number%10;
+        String[] nume_sute = {"", "o suta", "doua sute", "trei sute", "patru sute", "cinci sute", "sase sute", "sapte sute", "opt sute", "noua sute"};
+        String[] nume_zeci = {"", "", "douazeci", "treizeci", "patruzeci", "cincizeci", "saizeci", "saptezeci", "optzeci", "nouazeci"};
+        String[] nume_unitati = {"", "unu", "doi", "trei", "patru", "cinci", "sase", "sapte", "opt", "noua"};
+
+        if (sute != 0) {
+            System.out.print(nume_sute[sute] + " ");
+        }
+        if (zeci == 1){
+            System.out.println("zeci" + nume_unitati[unitati]);
+        } else  {
+            System.out.println(nume_zeci[zeci]+ " "+ nume_unitati[unitati]);
+        }
+
+    }
+}
